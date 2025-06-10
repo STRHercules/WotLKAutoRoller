ee314d02bd61da2c2686830220c676bb695c483d
STRHercules <32891881+STRHercules@users.noreply.github.com>
2025-06-09 20:43:26 -0500

    Guard menu frame access

--- Carbonite/Carbonite.lua
+++ Carbonite/Carbonite.lua
@@ -2,3 +2,6 @@ local GMF=_G.GameMenuFrame
 if win and (not GMF or not GMF:IsShown()) then
  local GMF=_G.GameMenuFrame
 if not (GMF and GMF:IsShown()) and not self.NoS then
+local GMF = _G.GameMenuFrame
+if win and not (GMF and GMF:IsShown()) then
+ local GMF = _G.GameMenuFrame
